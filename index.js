239a84f4bb9982c6445947060eecdee54e443ed3
jeremiah <jeremiah@jeremiahfoster.com>
2020-07-28 21:58:29 -0400

    2 files modified

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import "./style.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'foo      '
+      name: "foo      "
     };
   }
 
@@ -15,12 +15,10 @@ class App extends Component {
     return (
       <div>
         <Hello name={this.state.name} />
-        <p>
-          Now need to create a React app. :P
-        </p>
+        <p>Now need to create a React app. :P</p>
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
